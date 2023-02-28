3bb19b07fe62921ec795e98e368bdcef497736a6
Kaike Dias <kaikedias98@gmail.com>
2023-02-28 17:03:38 -0300

    add questions 13.2, 13.3, 13.4, 13.5

--- tree/ex_01.cpp
+++ tree/ex_01.cpp
@@ -38,8 +38,61 @@ void arvoreImprime(Tree root) {
     }
 }
 
+//Questao 13.2 (quantidade nós) 
+int qtdNos(Tree arvore) {
+    if(arvVazia(arvore)) {
+        return 0;
+    }
+
+    return qtdNos(arvore->left) + qtdNos(arvore->right) + 1;
+}
+
+//Questao 13.3 (quantidade de folhas)
+int qtdFolhas(Tree arvore) {
+    if(arvVazia(arvore)) {
+        return 0;
+    } else if(arvore->left == NULL && arvore->right == NULL) {
+        return 1;
+    }
+
+    return qtdFolhas(arvore->left) + qtdFolhas(arvore->right);
+}
+
+//Questao 13.4 (altura da arvore)
+int altura(Tree arvore) {
+    if(arvVazia(arvore) || (arvore->left == NULL && arvore->right ==NULL)) {
+        return 0;
+    } else {
+        int alturaEsquerda = altura(arvore->left);
+        int alturaDireita = altura(arvore->right);
+        if(alturaEsquerda > alturaDireita){
+            return alturaEsquerda + 1;
+        }else {
+            return alturaDireita + 1;
+        }
+    }
+
+
+}
+
+//Questao 13.5
+bool estaNaArvore(Tree arvore, int item) {
+    if(arvVazia(arvore)) {
+        return false;
+    }else if(arvore->value == item) {
+        return true;
+    }
+
+    return estaNaArvore(arvore->left, item) || estaNaArvore(arvore->right, item);
+}
+
 int main() {
     Tree arvore = criarArvore(1, criarArvoreVazia(), criarArvore(2, criarArvoreVazia(), criarArvore(3, criarArvoreVazia(), criarArvoreVazia())));
 
     arvoreImprime(arvore);
+    cout << "\n" << qtdNos(arvore) << endl;
+    cout << qtdFolhas(arvore) << endl;
+    cout << altura(arvore) << endl;
+    cout << estaNaArvore(arvore, 1) << endl;
+    cout << estaNaArvore(arvore, 5) << endl;
 }
