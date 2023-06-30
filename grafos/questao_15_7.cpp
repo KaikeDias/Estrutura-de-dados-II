935e5910156d3c9edbed2b3b798eef0e5adb4bf9
Kaike Dias <kaikedias98@gmail.com>
2023-06-30 12:42:12 -0300

    correcao

--- grafos/questao_15_7.cpp
+++ grafos/questao_15_7.cpp
@@ -4,11 +4,13 @@
 #include <list>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Graph {
     int numVertices;
     vector<int>* adjLists;
+    vector<string> disciplineNames;
     vector<int> inDegree;
 
 public:
@@ -16,76 +18,81 @@ public:
         numVertices = vertices;
         adjLists = new vector<int>[vertices];
         inDegree.resize(vertices, 0);
+        disciplineNames.resize(vertices);
     }
 
-    void addEdge(int src, int dest) {
+    void addEdge(int src, int dest, string disciplineName) {
         adjLists[src].push_back(dest);
         inDegree[dest]++;
+        disciplineNames[src] = disciplineName;
     }
 
     void topologicalSort() {
         queue<int> q;
+        vector<int> semester(numVertices, 0); // Armazena o número do semestre de cada disciplina
 
         // Inicializa a fila com os vértices que não têm arestas de entrada
         for (int i = 0; i < numVertices; i++) {
             if (inDegree[i] == 0) {
                 q.push(i);
+                semester[i] = 1; // Define o primeiro semestre para as disciplinas sem pré-requisitos
             }
         }
 
-        cout << "Ordenação topológica resultante:\n";
+        cout << "Disciplinas que podem ser feitas em um mesmo semestre:\n";
 
+        int currentSemester = 1;
         while (!q.empty()) {
-            int v = q.front();
-            q.pop();
+            int size = q.size();
+            cout << currentSemester << ": ";
 
-            cout << v << " ";
+            vector<string> semesterDisciplines;
 
-            // Remove o vértice atual e atualiza as contagens de grau de entrada dos vértices adjacentes
-            for (int adj : adjLists[v]) {
-                inDegree[adj]--;
+            for (int i = 0; i < size; i++) {
+                int v = q.front();
+                q.pop();
 
-                if (inDegree[adj] == 0) {
-                    q.push(adj);
+                semesterDisciplines.push_back(disciplineNames[v]);
+
+                // Atualiza o número do semestre para as disciplinas adjacentes
+                for (int adj : adjLists[v]) {
+                    inDegree[adj]--;
+
+                    if (inDegree[adj] == 0) {
+                        q.push(adj);
+                        semester[adj] = currentSemester + 1; // Define o próximo semestre para as disciplinas sem pré-requisitos restantes
+                    }
                 }
             }
-        }
 
-        cout << endl;
+            // Ordena as disciplinas em ordem alfabética
+            sort(semesterDisciplines.begin(), semesterDisciplines.end());
+
+            for (string discipline : semesterDisciplines) {
+                cout << discipline << ", ";
+            }
+
+            cout << endl;
+            currentSemester++;
+        }
     }
 };
 
 int main() {
-    int numVertices = 9;
+    int numVertices = 8;
     Graph graph(numVertices);
 
-    graph.addEdge(0, 1);
-    // Arquitetura de Computadores -> Algoritmos
-    graph.addEdge(1, 2);
-    // Algoritmos -> Linguagem de Programação
-    graph.addEdge(2, 3);
-    // Linguagem de Programação -> Programação Orientada a Objetos
-    graph.addEdge(2, 7);
-    // Linguagem de Programação -> Estrutura de Dados
-    graph.addEdge(3, 6);
-    // Programação Orientada a Objetos -> Banco de Dados
-    graph.addEdge(3, 7);
-    // Programação Orientada a Objetos -> Estrutura de Dados
-    graph.addEdge(4, 5);
-    // Matemática Discreta -> Cálculo I
-    graph.addEdge(5, 6);
-    // Cálculo I -> Cálculo II
-    graph.addEdge(6, 8);
-    // Cálculo II -> Cálculo Numérico Computacional
+    graph.addEdge(0, 1, "Arquitetura de Computadores");
+    graph.addEdge(1, 3, "Algoritmos");
+    graph.addEdge(1, 2, "Linguagem de Programação");
+    graph.addEdge(3, 4, "Programação Orientada a Objetos");
+    graph.addEdge(2, 5, "Estrutura de Dados");
+    graph.addEdge(2, 4, "Calculo I");
+    graph.addEdge(4, 6, "Calculo II");
+    graph.addEdge(6, 7, "Calculo Numerico Computacional");
+    graph.addEdge(5, 7, "Banco de Dados");
 
     graph.topologicalSort();
 
     return 0;
 }
-
-/*
-OUTPUT:
-
-Ordenação topológica resultante:
-0 4 1 5 2 3 6 7 8
-*/
